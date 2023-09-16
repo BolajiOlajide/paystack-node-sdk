df3598f504359d2640b68f0533d96ec2bea8938f
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-09-16 12:50:05 +0100

    remove validateStatus config

--- src/http/index.ts
+++ src/http/index.ts
@@ -12,10 +12,6 @@ export function createHTTPClient(baseURL: string, secretKey: string, config?: Ax
   const client = axios.create({
     // We don't want the baseURL and authorization config to be overwritten
     baseURL,
-    // We don't want to reject requests with a status code greater than 400.
-    // This is so that we can properly return the error message received from
-    // Paystack.
-    // validateStatus: (status: number): boolean => status < 500, // Resolve only if the status code is less than 500
 
     // The configuration above shouldn't be overwritten as they're required for the SDK
     // to work properly.
