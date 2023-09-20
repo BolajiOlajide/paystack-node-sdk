630ebef9df102d1fa6f65c7dac8d9f32a24465b3
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-09-20 11:45:14 -0400

    cleanup examples

--- examples/customer.example.ts
+++ examples/customer.example.ts
@@ -1,10 +1,26 @@
+import { argv } from 'process';
+
 import { ps } from './paystack';
 
-// This will create a new plan for subscriptions.
-// If there's an error it'll log the error message to the console.
-// ps.customer
-//   .create({ email: 'test@gmail.com' })
-//   .then(console.log)
-//   .catch((err) => console.error(err));
+if (argv.length < 3) {
+  throw new Error('incorrect usage');
+}
 
-ps.customer.list({}).then(console.log).catch(console.error);
+const [_, __, command] = argv;
+
+switch (command) {
+  case 'create':
+    ps.customer
+      .create({ email: 'test@gmail.com' })
+      .then(console.log)
+      .catch((err) => console.error(err));
+    break;
+  case 'list':
+    ps.customer.list({}).then(console.log).catch(console.error);
+    break;
+  case 'get':
+    ps.customer.get({ email_or_code: 'CUS_xzp56gco8omykrm' }).then(console.log).catch(console.error);
+    break;
+  default:
+    console.error('invalid command');
+}
