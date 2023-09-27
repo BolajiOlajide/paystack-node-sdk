0020b9d39a93e4a8bd8f7667d78137d2c9023af4
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2023-09-26 21:03:32 -0400

    add bank fixtures

--- src/fixtures/country.fixture.ts
+++ src/fixtures/country.fixture.ts
@@ -1,72 +1,75 @@
 import type { Country } from '../schema/country.schema';
-const mockCountries: Country[] = [
-  {
-    id: 1,
-    active_for_dashboard_onboarding: true,
-    name: 'Nigeria',
-    iso_code: 'NG',
-    default_currency_code: 'NGN',
-    integration_defaults: {},
-    calling_code: '+234',
-    pilot_mode: false,
-    relationships: {
-      currency: {
-        type: 'currency',
-        data: ['NGN'],
-        supported_currencies: {
-          NGN: {
-            bank: {
-              bank_type: 'nuban',
-              branch_code: false,
-              branch_code_type: 'text',
-              account_name: false,
-              account_verification_required: true,
-              account_number_label: 'accountNumber',
-              account_number_pattern: {
-                exact_match: true,
-                pattern: '[0-9]{10}',
-              },
-              documents: [],
-              show_account_number_tooltip: false,
+
+export const mockCountry = {
+  id: 1,
+  active_for_dashboard_onboarding: true,
+  name: 'Nigeria',
+  iso_code: 'NG',
+  default_currency_code: 'NGN',
+  integration_defaults: {},
+  calling_code: '+234',
+  pilot_mode: false,
+  relationships: {
+    currency: {
+      type: 'currency',
+      data: ['NGN'],
+      supported_currencies: {
+        NGN: {
+          bank: {
+            bank_type: 'nuban',
+            branch_code: false,
+            branch_code_type: 'text',
+            account_name: false,
+            account_verification_required: true,
+            account_number_label: 'accountNumber',
+            account_number_pattern: {
+              exact_match: true,
+              pattern: '[0-9]{10}',
             },
+            documents: [],
+            show_account_number_tooltip: false,
           },
-          USD: {
-            bank: {
-              bank_type: 'nuban',
-              required_fields: ['bank', 'account_number'],
-              branch_code: false,
-              branch_code_type: 'text',
-              account_name: false,
-              account_verification_required: true,
-              account_number_label: 'accountNumber',
-              account_number_pattern: {
-                exact_match: true,
-                pattern: '',
-              },
-              documents: [],
-              notices: [
-                'USD can currently only be settled into Zenith Bank USD accounts.',
-                "You'll receive USD settlements 7 working days after payment has been made.",
-                'In case of fraudulent transactions involving international payments, Paystack will reverse the transaction and deduct the amount from your balance.',
-              ],
+        },
+        USD: {
+          bank: {
+            bank_type: 'nuban',
+            required_fields: ['bank', 'account_number'],
+            branch_code: false,
+            branch_code_type: 'text',
+            account_name: false,
+            account_verification_required: true,
+            account_number_label: 'accountNumber',
+            account_number_pattern: {
+              exact_match: true,
+              pattern: '',
             },
+            documents: [],
+            notices: [
+              'USD can currently only be settled into Zenith Bank USD accounts.',
+              "You'll receive USD settlements 7 working days after payment has been made.",
+              'In case of fraudulent transactions involving international payments, Paystack will reverse the transaction and deduct the amount from your balance.',
+            ],
           },
         },
       },
-      integration_feature: {
-        type: 'integration_feature',
-        data: [],
-      },
-      integration_type: {
-        type: 'integration_type',
-        data: ['ITYPE_001', 'ITYPE_002', 'ITYPE_003'],
-      },
-      payment_method: {
-        type: 'payment_method',
-        data: ['PAYM_001', 'PAYM_002', 'PAYM_003', 'PAYM_004'],
-      },
+    },
+    integration_feature: {
+      type: 'integration_feature',
+      data: [],
+    },
+    integration_type: {
+      type: 'integration_type',
+      data: ['ITYPE_001', 'ITYPE_002', 'ITYPE_003'],
+    },
+    payment_method: {
+      type: 'payment_method',
+      data: ['PAYM_001', 'PAYM_002', 'PAYM_003', 'PAYM_004'],
     },
   },
+};
+
+export const mockCountries: Country[] = [
+  mockCountry,
   {
     id: 2,
     active_for_dashboard_onboarding: true,
