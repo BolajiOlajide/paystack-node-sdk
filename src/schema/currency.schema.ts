cfa988c270de1ef54101bc2dfdbdfe38624b6df1
Bolaji Olajide <25608335+BolajiOlajide@users.noreply.github.com>
2024-10-27 16:07:53 +0000

    fix lint

--- src/schema/currency.schema.ts
+++ src/schema/currency.schema.ts
@@ -3,4 +3,5 @@ import { z } from 'zod';
 export const currencySchema = z.enum(['NGN', 'USD', 'GHS', 'ZAR', 'KES'], {
   invalid_type_error: 'currency must be one of NGN, USD, GHS, ZAR or KES',
 });
-type Currency = z.infer<typeof currencySchema>;
+
+export type Currency = z.infer<typeof currencySchema>;
